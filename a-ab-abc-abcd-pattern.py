ff6833f263bb8f149704645340795bca9ef0e8a9
20VC0913 <70229518+20VC0913@users.noreply.github.com>
2022-05-03 10:12:53 +0530

    Create a-ab-abc-abcd-pattern.py

---
+++ a-ab-abc-abcd-pattern.py
@@ -0,0 +1,10 @@
+# a-ab-abc-abcd-abcde Pattern up to n lines
+
+n = int(input("Enter number of rows: "))
+
+for i in range(1,n+1):
+    A = 97
+    for j in range(1, i+1):
+        print("%c" %(A), end="")
+        A += 1
+    print()
